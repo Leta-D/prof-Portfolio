ebe09932cf5599e00b1a3e27cf8b01cb0766c933
Leta-D <letadeju.1278@gmail.com>
2025-09-02 23:49:43 +0300

    improvment addd

--- script.js
+++ script.js
@@ -3,7 +3,6 @@ let isDarkMode = true;
 function themeModeHandler(){
   isDarkMode = !isDarkMode;
   document.getElementById("lightDarkMode").querySelector("#social-icons-img").src = isDarkMode? "/assets/icons/sun.png" : "/assets/icons/moon.png";
-  alert(isDarkMode ? "Theme changed to Dark mode" : "Theme changed to Light mode");
 }
   
 
